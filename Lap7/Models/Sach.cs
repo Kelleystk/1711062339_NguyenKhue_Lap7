0321171257131daf4c5a490108d8175bd34ae36c
KelleysTK <KelleysTK@KELLEYSTK-PC>
2020-06-18 00:57:59 +0700

    up data

--- Lap7/Models/Sach.cs
+++ Lap7/Models/Sach.cs
@@ -7,6 +7,10 @@ namespace Lap7.Models
 {
     public class Sach
     {
-        public 
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string AuthorName { get; set; }
+        public decimal Price { get; set; }
     }
 }
